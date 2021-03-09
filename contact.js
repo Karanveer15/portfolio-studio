618d26da1b8a455951f4a21198ebce29a496d202
Karanveer15 <80299555+Karanveer15@users.noreply.github.com>
2021-03-08 21:54:56 -0500

    Add files via upload
    [KV]porfolio-studio

---
+++ contact.js
@@ -0,0 +1,48 @@
+﻿var $ = function(id) 
+{ 
+return document.getElementById(id);
+};
+var validate = function()
+{
+	        let name=document.getElementById('name').value.trim();
+            let email=document.getElementById('email').value.trim();
+            let number=document.getElementById('number').value.trim();
+            let location=document.getElementById('location').value.trim();
+			let select=document.getElementById('select').value.trim();
+            if(name=='' )
+			{
+                alert('Please enter your full name');
+                return false;
+            }
+			if(email=='')
+			{
+				alert("Please enter your email ");
+				return false;
+			}
+			 if(number=='')
+			{
+				alert("Please enter your working mobile number");
+				return false;
+			}
+			if(location=='')
+			{
+				alert("Please enter a location of wedding");
+				return false;
+			}
+			if(select=='Select an Option')
+			{
+				alert("Please choose an event");
+				return false;
+			}
+			else
+			{
+                alert('Thank you!. We will contact you soon.');
+                return true;
+            }
+};
+window.onload = function()
+{
+	$("submit").onclick = validate;
+	$("name").focus();
+};
+
